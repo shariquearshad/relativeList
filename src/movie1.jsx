79a9f2bfc452305cae1487e29e6a02adb265924d
shariquearshad <shariquearshad@gmail.com>
2020-12-13 18:32:35 +0530

    change 2

--- src/movie1.jsx
+++ src/movie1.jsx
@@ -4,10 +4,10 @@ import "./styles.css";
 export default function Movie1() {
   return (
     <>
-      <ol key={"relativeList"}>
-        <li key={"relativeListItem1"}>relative1</li>
-        <li key={"relativeListItem2"}>relative2</li>
-        <li key={"relativeListItem3"}>relative3</li>
+      <ol key="relativeList">
+        <li key="relativeListItem1">relative1</li>
+        <li key="relativeListItem2">relative2</li>
+        <li key="relativeListItem3">relative2</li>
       </ol>
     </>
   );
